934d27ce11c83e3e7dc5fd68605636df2ee0113c
Jeff Irion <jefflirion@users.noreply.github.com>
2018-11-04 14:58:16 -0800

    Initial async attempt (not working)

--- media_player/firetv.py
+++ media_player/firetv.py
@@ -4,10 +4,11 @@ Support for functionality to interact with FireTV devices.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/media_player.firetv/
 """
+import asyncio
+import datetime
 import functools
 import logging
 import os
-import time
 import voluptuous as vol
 
 from homeassistant.components.media_player import (
@@ -18,6 +19,7 @@ from homeassistant.const import (
     STATE_IDLE, STATE_OFF, STATE_PAUSED, STATE_PLAYING, STATE_STANDBY,
     STATE_UNKNOWN, CONF_HOST, CONF_NAME, CONF_PORT)
 import homeassistant.helpers.config_validation as cv
+from homeassistant.util import Throttle
 
 REQUIREMENTS = ['firetv==1.0.6']
 
@@ -49,6 +51,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 PACKAGE_LAUNCHER = "com.amazon.tv.launcher"
 PACKAGE_SETTINGS = "com.amazon.tv.settings"
+MIN_TIME = datetime.timedelta(milliseconds=10)
+SLEEP_TIME = 0.1
 
 
 # pylint: disable=protected-access
@@ -86,39 +90,30 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         add_devices([device])
 
 
-def adb_wrapper(func):
+@Throttle(MIN_TIME)
+def adb_command(command, is_property=False):
     """Wait if previous ADB commands haven't finished."""
-    @functools.wraps(func)
-    def _adb_wrapper(self, *args, **kwargs):
-        attempts = 0
-        while self._adb_lock and attempts < 5:
-            attempts += 1
-            time.sleep(1)
-
-        if attempts == 5 and self._adb_lock:
-            try:
-                self._firetv.connect()
-            except self._exceptions:
-                _LOGGER.error('Failed to re-establish the ADB connection; '
-                              'will re-attempt in the next update.')
-                self._firetv._adb = None
-                self._adb_lock = False
-                return
-
-        self._adb_lock = True
+    @functools.wraps(command)
+    def _adb_command(self, *args, **kwargs):
+        # try to run the ADB command
         try:
-            returns = func(self, *args, **kwargs)
+            if is_property:
+                # the command is a property
+                returns = getattr(self, command)
+            else:
+                # the command is a method
+                returns = command(self, *args, **kwargs)
+
+        # catch ADB-related exceptions
         except self._exceptions:
-            returns = None
             _LOGGER.error('Failed to execute an ADB command; will attempt to '
                           're-establish the ADB connection in the next update')
+            returns = None
             self._firetv._adb = None
-        finally:
-            self._adb_lock = False
 
         return returns
 
-    return _adb_wrapper
+    return _adb_command
 
 
 class FireTVDevice(MediaPlayerDevice):
@@ -136,8 +131,8 @@ class FireTVDevice(MediaPlayerDevice):
         self._adb_lock = False
 
         self._exceptions = (TypeError, ValueError, AttributeError,
-                            InvalidCommandError, InvalidResponseError,
-                            InvalidChecksumError)
+                            BrokenPipeError, InvalidCommandError,
+                            InvalidResponseError, InvalidChecksumError)
 
         self._name = name
         self._state = STATE_UNKNOWN
@@ -176,8 +171,7 @@ class FireTVDevice(MediaPlayerDevice):
         """Return a list of running apps."""
         return self._running_apps
 
-    @adb_wrapper
-    def update(self):
+    async def async_update(self):
         """Get the latest date and update device state."""
         # Check if device is disconnected.
         if not self._firetv._adb:
@@ -186,113 +180,135 @@ class FireTVDevice(MediaPlayerDevice):
             self._current_app = None
 
             # Try to connect
-            self._firetv.connect()
+            adb_command(type(self._firetv).connect)(self)
+            await asyncio.sleep(SLEEP_TIME)
+            return
 
         # Check if device is off.
-        elif not self._firetv._screen_on:
+        if not adb_command('_screen_on', is_property=True)(self):
+            await asyncio.sleep(SLEEP_TIME)
             self._state = STATE_OFF
             self._running_apps = None
             self._current_app = None
+            return
+
+        # Sleep after checking `_screen_on`.
+        await asyncio.sleep(SLEEP_TIME)
 
         # Check if screen saver is on.
-        elif not self._firetv._awake:
+        if not adb_command('_awake', is_property=True)(self):
+            await asyncio.sleep(SLEEP_TIME)
             self._state = STATE_IDLE
             self._running_apps = None
             self._current_app = None
+            return
 
-        else:
-            # Get the running apps.
-            if self._get_sources:
-                self._running_apps = self._firetv.running_apps()
+        # Sleep after checking `_awake`.
+        await asyncio.sleep(SLEEP_TIME)
 
-            # Get the current app.
-            if self._get_source:
-                current_app = self._firetv.current_app
-                if isinstance(current_app, dict)\
-                        and 'package' in current_app:
-                    self._current_app = current_app['package']
-                else:
-                    self._current_app = current_app
+        # Get the running apps.
+        if self._get_sources:
+            self._running_apps =\
+                adb_command(type(self._firetv).running_apps)(self)
+            await asyncio.sleep(SLEEP_TIME)
 
-                # Show the current app as the only running app.
-                if not self._get_sources:
-                    if self._current_app:
-                        self._running_apps = [self._current_app]
-                    else:
-                        self._running_apps = None
+        # Get the current app.
+        if self._get_source:
+            current_app = adb_command('current_app', is_property=True)(self)
+            await asyncio.sleep(SLEEP_TIME)
 
-                # Check if the launcher is active.
-                if self._current_app in [PACKAGE_LAUNCHER,
-                                         PACKAGE_SETTINGS]:
-                    self._state = STATE_STANDBY
-
-                # Check for a wake lock (device is playing).
-                elif self._firetv._wake_lock:
-                    self._state = STATE_PLAYING
-
-                # Otherwise, device is paused.
-                else:
-                    self._state = STATE_PAUSED
-
-            # Don't get the current app.
-            elif self._firetv._wake_lock:
-                # Check for a wake lock (device is playing).
-                self._state = STATE_PLAYING
+            if isinstance(current_app, dict) and 'package' in current_app:
+                self._current_app = current_app['package']
             else:
-                # Assume the devices is on standby.
+                self._current_app = current_app
+
+            # Show the current app as the only running app.
+            if not self._get_sources:
+                if self._current_app:
+                    self._running_apps = [self._current_app]
+                else:
+                    self._running_apps = None
+
+            # Check if the launcher is active.
+            if self._current_app in [PACKAGE_LAUNCHER,
+                                     PACKAGE_SETTINGS]:
                 self._state = STATE_STANDBY
 
-    @adb_wrapper
+            # Check for a wake lock (device is playing).
+            elif adb_command('_wake_lock', is_property=True)(self):
+                self._state = STATE_PLAYING
+
+            # Otherwise, device is paused.
+            else:
+                self._state = STATE_PAUSED
+
+            # Sleep after checking `_wake_lock`.
+            await asyncio.sleep(SLEEP_TIME)
+
+            return
+
+        # Don't get the current app.
+        if adb_command('_wake_lock', is_property=True)(self):
+            # Check for a wake lock (device is playing).
+            self._state = STATE_PLAYING
+        else:
+            # Assume the devices is on standby.
+            self._state = STATE_STANDBY
+
+        # Sleep after checking `_wake_lock`.
+        await asyncio.sleep(SLEEP_TIME)
+
+    @adb_command
     def turn_on(self):
         """Turn on the device."""
         self._firetv.turn_on()
 
-    @adb_wrapper
+    @adb_command
     def turn_off(self):
         """Turn off the device."""
         self._firetv.turn_off()
 
-    @adb_wrapper
+    @adb_command
     def media_play(self):
         """Send play command."""
         self._firetv.media_play()
 
-    @adb_wrapper
+    @adb_command
     def media_pause(self):
         """Send pause command."""
         self._firetv.media_pause()
 
-    @adb_wrapper
+    @adb_command
     def media_play_pause(self):
         """Send play/pause command."""
         self._firetv.media_play_pause()
 
-    @adb_wrapper
+    @adb_command
     def media_stop(self):
         """Send stop (back) command."""
         self._firetv.back()
 
-    @adb_wrapper
+    @adb_command
     def volume_up(self):
         """Send volume up command."""
         self._firetv.volume_up()
 
-    @adb_wrapper
+    @adb_command
     def volume_down(self):
         """Send volume down command."""
         self._firetv.volume_down()
 
-    @adb_wrapper
+    @adb_command
     def media_previous_track(self):
         """Send previous track command (results in rewind)."""
         self._firetv.media_previous()
 
-    @adb_wrapper
+    @adb_command
     def media_next_track(self):
         """Send next track command (results in fast-forward)."""
         self._firetv.media_next()
 
-    @adb_wrapper
+    @adb_command
     def select_source(self, source):
         """Select input source.
 
