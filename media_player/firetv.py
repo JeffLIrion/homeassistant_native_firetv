3e9e1bb4352c36b9fb8dec3bec7140157e728247
Jeff Irion <JeffLIrion@users.noreply.github.com>
2019-02-10 08:29:20 -0800

    Return None for properties when unavailable

--- media_player/firetv.py
+++ media_player/firetv.py
@@ -225,6 +225,8 @@ class FireTVDevice(MediaPlayerDevice):
     @property
     def state(self):
         """Return the state of the player."""
+        if not self._available:
+            return None
         return self._state
 
     @property
@@ -235,16 +237,22 @@ class FireTVDevice(MediaPlayerDevice):
     @property
     def app_id(self):
         """Return the current app."""
+        if not self._available:
+            return None
         return self._current_app
 
     @property
     def source(self):
         """Return the current app."""
+        if not self._available:
+            return None
         return self._current_app
 
     @property
     def source_list(self):
         """Return a list of running apps."""
+        if not self._available:
+            return None
         return self._running_apps
 
     @adb_decorator(override_available=True)
@@ -252,9 +260,6 @@ class FireTVDevice(MediaPlayerDevice):
         """Update the device state and, if necessary, re-connect."""
         # Check if device is disconnected.
         if not self._available:
-            self._running_apps = None
-            self._current_app = None
-
             # Try to connect
             self._available = self.firetv.connect()
 
